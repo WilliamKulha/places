cbdbbfb93b9c7af16f1047cc5de7c0143a5eb1e8
William Kulha <kulha.william@gmail.com>
2018-10-11 18:09:10 -0400

    Add comments so code is easier for FM to understand.

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,17 @@ function Destination(location, pointOfInterest, timeofyear, note, rating) {
 $(document).ready(function() {
   $('#destination_form').submit(function(event) {
     event.preventDefault();
-
+    //Set variables to hold all form inputs.
     let place = $('#place').val();
     let landmarks = $('#landmarks').val();
     let tOY = $('#toy').val();
     let memories = $('#notes').val();
     let rating = $('#rating').val();
-
+    //Instantiate a new Destination object, passing it all the values stored in the variables.
     let newDestination = new Destination(place, landmarks, tOY, memories, rating);
+    //Add each new Destination to the list, showing only its name.
     $('#destinations_list').append('<li><span class="entry">' + newDestination.location + '</span></li>')
+    //Make it so that whichever entry was clicked last is shown in the expanded view.
     $('.entry').last().click(function() {
       $('#show-entry').slideDown();
       $('#show-entry h2').text(newDestination.location.toUpperCase());
@@ -29,6 +31,7 @@ $(document).ready(function() {
       $('.expand_notes').text(newDestination.notes);
       $('.expand_rating').text(newDestination.rating);
     });
+    //Make all the form entries blank.
     $('#place').val('');
     $('#landmarks').val('');
     $('#toy').val('');
